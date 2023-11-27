b83de52e7c2fe2f50276874f05e9e84ec8f5c914
Remmy <remremegg@gmail.com>
2023-11-27 16:51:06 -0700

    Version 1.4.1 - Forgot to include unversioned files

---
+++ src/main/gen/mitsuko_plugin/psi/impl/MitsukoAtLabelImpl.java
@@ -0,0 +1,30 @@
+// This is a generated file. Not intended for manual editing.
+package mitsuko_plugin.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static mitsuko_plugin.psi.MitsukoTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import mitsuko_plugin.psi.*;
+
+public class MitsukoAtLabelImpl extends ASTWrapperPsiElement implements MitsukoAtLabel {
+
+  public MitsukoAtLabelImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull MitsukoVisitor visitor) {
+    visitor.visitAtLabel(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof MitsukoVisitor) accept((MitsukoVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+}
